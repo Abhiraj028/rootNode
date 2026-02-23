2c06c89132f53a20d038ba74b8dbda6673f380d8
Abhiraj028 <abhirajgautam028@gmail.com>
2026-02-23 23:16:57 +0530

    workspace routes

--- backend/src/interfaces/workspaceInterfaces.ts
+++ backend/src/interfaces/workspaceInterfaces.ts
@@ -3,6 +3,16 @@ import z from "zod";
 export const workspaceCreationSchema = z.object({
     name: z.string().max(100, "Workspace name should be less than 100 characters").trim()
 });
-
 export type workspaceCreationInterface = z.infer<typeof workspaceCreationSchema>;
 
+export const workspaceUpdateSchema = z.object({
+    name: z.string().min(1, "Workspace name cannot be empty").max(100, "Workspace name should be less than 100 characters").trim()
+});
+export type workspaceUpdateInterface = z.infer<typeof workspaceUpdateSchema>;
+
+export const workspaceDeleteParams = z.object({
+    workspaceId : z.string().min(1, "Workspace ID is required")
+});
+export type workspaceDeleteParamsInterface = z.infer<typeof workspaceDeleteParams>;
+
+export type workspaceUpdateParamsInterface = z.infer<typeof workspaceDeleteParams>;
--- backend/src/routes/workspace.routes.ts
+++ backend/src/routes/workspace.routes.ts
@@ -1,10 +1,12 @@
 import { Router, Request, Response } from "express";
 import { poolClient } from "../db";
 import { DatabaseError } from "pg";
-import { workspaceCreationInterface, workspaceCreationSchema } from "../interfaces/workspaceInterfaces";
+import { workspaceCreationInterface, workspaceCreationSchema, workspaceDeleteParamsInterface, workspaceUpdateInterface, workspaceUpdateParamsInterface, workspaceUpdateSchema} from "../interfaces/workspaceInterfaces";
 import crypto from "crypto";
 import { orgMiddleware } from "../middlewares/orgMiddleware";
 import { AuthMiddleware } from "../middlewares/authMiddleware";
+import Roles from "../root";
+import { number } from "zod";
 
 const router = Router();
 
@@ -41,6 +43,90 @@ router.post("/create", AuthMiddleware ,orgMiddleware , async(req: Request<{},{},
             return res.status(409).json({message: "Workspace with the same name already exists"});
         }
         console.log("Error creating workspace with name: "+workspaceName+" for orgId: "+orgId+" by userId: "+userId, err);
+        return res.status(500).json({message: "Internal server error"});
+    }
+});
+
+router.get("/", AuthMiddleware, orgMiddleware, async(req: Request<workspaceDeleteParamsInterface>, res: Response) => {
+    if(!req.user || !req.user.orgId){
+        return res.status(401).json({message: "Unauthorised"});
+    }
+    const {orgId} = req.user;
+    try{
+        const workspaceQuery = await poolClient.query("select id, name from workspaces where org_id = $1 and deleted_at is null", [orgId]);
+        return res.status(200).json({message: "Workspaces fetched successfully", data: workspaceQuery.rows});
+    }catch(err){
+        console.log("Error fetching workspaces for orgId: "+orgId, err);
+        return res.status(500).json({message: "Internal server error"});
+    }
+});
+
+router.delete("/:workspaceId", AuthMiddleware, orgMiddleware, async(req: Request, res: Response) => {
+    if(!req.user || !req.user.userId || !req.user.orgId || !req.user.orgRole){
+        return res.status(401).json({message: "Unauthorised"});
+    }
+
+    const {userId, orgId, orgRole} = req.user;
+    
+    const workspaceId = Number(req.params.workspaceId);
+
+    if(isNaN(workspaceId) || workspaceId <= 0){
+        return res.status(400).json({message: "Invalid workspaceId"});
+    }
+
+    if(orgRole != Roles.ADMIN){
+        return res.status(403).json({message: "Forbidden access"});
+    }
+
+    try{
+        const deleteQuery = await poolClient.query("update workspaces set deleted_at = now(), deleted_by = $1 where id = $2 and org_id = $3 returning id", [userId, workspaceId, orgId]);
+        if(deleteQuery.rowCount == 0){
+            return res.status(404).json({message: "Workspace not found"});
+        }
+        console.log("Workspace with id: "+workspaceId+" has been deleted by userId: "+userId);
+        return res.status(200).json({message: "Workspace deleted successfully"});
+    }catch(err){
+        if(err instanceof DatabaseError && err.code === "23503"){
+            console.log("Attempted to delete workspace with id: "+workspaceId+" which has dependent records, by userId: "+userId);
+            return res.status(409).json({message: "Cannot delete workspace with dependent records"});
+        }
+        console.log("Error deleting workspace with id: "+workspaceId+" by userId: "+userId, err);
+        return res.status(500).json({message: "Internal server error"});
+    }
+});
+
+router.patch("/:workspaceId", AuthMiddleware, orgMiddleware, async(req: Request<workspaceUpdateParamsInterface,{},workspaceUpdateInterface>, res:Response) => {
+    if(!req.user || !req.user.userId || !req.user.orgId || !req.user.orgRole){
+        return res.status(401).json({message: "Unauthorised"});
+    }
+    const {userId, orgId, orgRole} = req.user;
+    if(orgRole == Roles.MEMBER){
+        return res.status(403).json({message: "Forbidden access"});
+    }
+    const workspaceId = Number(req.params.workspaceId);
+    if(isNaN(workspaceId) || workspaceId <= 0){
+        return res.status(400).json({message: "Invalid workspaceId"});
+    }
+
+    const parsedData = workspaceUpdateSchema.safeParse(req.body);
+    if(!parsedData.success){
+        return res.status(400).json({message: "Invalid inputs", error: parsedData.error.message});
+    }
+    const workspaceName = parsedData.data.name;
+
+    try{
+        const updateWorkspaceQuery = await poolClient.query("update workspaces set name = $1, updated_at = now() where id = $2 and org_id = $3 and deleted_at is null returning id", [workspaceName, workspaceId, orgId]);
+        if(updateWorkspaceQuery.rowCount == 0){
+            return res.status(404).json({message: "Workspace not found"});
+        }
+        console.log("Workspace with id: "+workspaceId+" has been updated to name: "+workspaceName+" by userId: "+userId);
+        return res.status(200).json({message: "Workspace updated successfully"});
+    }catch(err){
+        if(err instanceof DatabaseError && err.code === "23505"){
+            console.log("Workspace with the same name already exists for orgId: "+orgId+", attempted to update workspace with id: "+workspaceId+" to name: "+workspaceName+" by userId: "+userId);
+            return res.status(409).json({message: "Workspace with the same name already exists"});
+        }
+        console.log("Error updating workspace with id: "+workspaceId+" to name: "+workspaceName+" by userId: "+userId, err);
         return res.status(500).json({message: "Internal server error"});
     }
 });
