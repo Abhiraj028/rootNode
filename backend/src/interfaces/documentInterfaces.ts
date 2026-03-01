bd1fbfbded758091e3cbab370c695a870e905d44
Abhiraj028 <abhirajgautam028@gmail.com>
2026-03-01 19:34:58 +0530

    doc routes fixed

--- backend/src/interfaces/documentInterfaces.ts
+++ backend/src/interfaces/documentInterfaces.ts
@@ -17,3 +17,19 @@ export type deleteDocParamsInterface = {
     docId: string;
     workspaceId: string;
 }
+
+export type updateDocParamsInterface = {
+    docId: string;
+    workspaceId: string;
+    parentId? : string;
+};
+
+export type getDocParamsInterface = createDocParamsInterface;
+
+export const updateDocSchema = z.object({
+    name: z.string().min(1, "Document name cannot be empty").max(100, "Document name should be less than 100 characters").trim().optional(),
+    title: z.string().min(1, "Document title cannot be empty").max(100, "Document title should be less than 100 characters").trim().optional(),
+    content: z.string().min(1, "Document content cannot be empty").trim().optional()
+});
+
+export type updateDocInterface = z.infer<typeof updateDocSchema>;
--- backend/src/routes/documents.routes.ts
+++ backend/src/routes/documents.routes.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from "express";
-import { createDocInterface, createDocParamsInterface, createDocSchema, deleteDocParamsInterface } from "../interfaces/documentInterfaces";
+import { createDocInterface, createDocParamsInterface, createDocSchema, deleteDocParamsInterface, updateDocInterface, updateDocParamsInterface, updateDocSchema } from "../interfaces/documentInterfaces";
 import { poolClient } from "../db";
 import crypto from "crypto";
 import { DatabaseError } from "pg";
@@ -9,7 +9,7 @@ import Roles from "../root";
 
 const router = Router();
 
-router.post("/create/:workspaceId/:parentId?", AuthMiddleware ,orgMiddleware , async(req: Request<createDocParamsInterface,{}, createDocInterface>, res: Response) => {
+router.post("/:workspaceId/:parentId?", AuthMiddleware ,orgMiddleware , async(req: Request<createDocParamsInterface,{}, createDocInterface>, res: Response) => {
     if(!req.user || !req.user.userId || !req.user.orgId || !req.user.orgRole){
         return res.status(400).json({message: "Invalid user data in request"});
     }
@@ -68,7 +68,7 @@ router.post("/create/:workspaceId/:parentId?", AuthMiddleware ,orgMiddleware , a
 });
 
 
-router.delete("/delete/:workspaceId/:docId", AuthMiddleware, orgMiddleware, async(req: Request<deleteDocParamsInterface>, res: Response) => {
+router.delete("/:workspaceId/:parentId?/:docId", AuthMiddleware, orgMiddleware, async(req: Request<deleteDocParamsInterface>, res: Response) => {
     if(!req.user || !req.user.userId || !req.user.orgId || !req.user.orgRole){
         return res.status(401).json({message: "Invalid user data in request"});
     }
@@ -85,12 +85,8 @@ router.delete("/delete/:workspaceId/:docId", AuthMiddleware, orgMiddleware, asyn
 
     const {userId, orgId, orgRole} = req.user;
 
-    const workspaceCheck = await poolClient.query("select org_id from workspaces where id = $1",[workspaceId]);
-    if(workspaceCheck.rowCount == 0 || workspaceCheck.rows[0].org_id != orgId){
-        return res.status(403).json({message: "Forbidden"});
-    }
 
-    const docCheck = await poolClient.query("select org_id, created_by from documents where id = $1 and workspace_id = $2", [docId, workspaceId]);
+    const docCheck = await poolClient.query("select org_id, created_by from documents where id = $1 and workspace_id = $2 and org_id = $3", [docId, workspaceId, orgId]);
     if(docCheck.rowCount == 0 || docCheck.rows[0].org_id != orgId){
         return res.status(403).json({message: "Forbidden"});
     }
@@ -114,6 +110,107 @@ router.delete("/delete/:workspaceId/:docId", AuthMiddleware, orgMiddleware, asyn
         return res.status(500).json("Some error occured");
     }
 
+});
+
+router.get("/:workspaceId/:parentId?", AuthMiddleware, orgMiddleware, async(req: Request, res: Response) => {
+    if(!req.user){
+        return res.status(401).json({message: "Invalid user data in request"});
+    }
+    const {orgId} = req.user;
+    const {workspaceId, parentId} = req.params;
+    if(!Number.isInteger(Number(workspaceId)) || Number(workspaceId) <= 0){
+        return res.status(400).json({message: "Invalid workspace id entered"});
+    }
+
+    const paramsWorkspaceId = Number(workspaceId);
+    const paramsParentId = !Number.isInteger(Number(parentId)) ? null : Number(parentId); 
+
+    try{
+        if(paramsParentId == null){
+            const docQuery = await poolClient.query("select * from documents where workspace_id = $1 and parent_id is null and org_id = $2", [paramsWorkspaceId, orgId]);
+            return res.status(200).json({message: "Documents fetched successfully", data: docQuery.rows});
+        }
+        const docQuery = await poolClient.query("select * from documents where workspace_id = $1 and parent_id = $2 and org_id = $3", [paramsWorkspaceId, paramsParentId, orgId]);
+        return res.status(200).json({message: "Documents fetched successfully", data: docQuery.rows});
+    }catch(err){
+        console.log("Error occured in fetching documents. "+err);
+        return res.status(500).json({message: "Some error occured"});
+    }
+});
+
+
+router.patch("/:workspaceId/:parentId?/:docId", AuthMiddleware, orgMiddleware, async(req: Request<updateDocParamsInterface, {}, updateDocInterface>, res:Response) => {
+    if(!req.user){
+        return res.status(401).json({message: "Invalid user data in request"});
+    }
+    const {userId, orgId, orgRole} = req.user;
+    const {workspaceId, parentId, docId} = req.params;
+    if(!Number.isInteger(Number(workspaceId)) || !Number.isInteger(Number(docId))){
+        return res.status(400).json({message: "Invalid workspace or document id entered"});
+    }
+    const numWorkspaceId = Number(workspaceId);
+    const numParentId = !Number.isInteger(Number(parentId)) ? null : Number(parentId);
+    const numDocumentId = Number(docId);
+
+    const parsedData = updateDocSchema.safeParse(req.body);
+    if(!parsedData.success){
+        return res.status(400).json({message: "Invalid inputs", error: parsedData.error.message});
+    }
+
+    let {name, title, content} = parsedData.data;
+
+    const userCheck = await poolClient.query("select created_by from documents where id = $1 and org_id = $2 and workspace_id = $3", [numDocumentId, orgId, numWorkspaceId]);
+    if(userCheck.rowCount == 0){
+        return res.status(404).json({message: "Document not found"});
+    }
+
+    if(orgRole == Roles.MEMBER && userCheck.rows[0].created_by != userId){
+        return res.status(403).json({message: "Forbidden"});
+    }
+    
+    try{
+        const updates = [];
+        const values = [];
+        let index = 1;
+
+        if (name !== undefined) {
+        updates.push(`name = $${index++}`);
+        values.push(name);
+        }
+        if (title !== undefined) {
+        updates.push(`title = $${index++}`);
+        values.push(title);
+        }
+        if (content !== undefined) {
+        updates.push(`content = $${index++}`);
+        values.push(content);
+        }
+
+        if (updates.length === 0) {
+        return res.status(400).json({ message: "No fields provided" });
+        }
+
+        values.push(numDocumentId, numWorkspaceId, orgId);
+
+        const query = `update documents set ${updates.join(", ")}, updated_at = now() where id = $${index++} and workspace_id = $${index++} and org_id = $${index} and deleted_at is null returning *`;
+
+        const updateQuery = await poolClient.query(query, values);
+        if(updateQuery.rowCount == 0){
+            return res.status(404).json({message: "Document not found or no changes made"});
+        }
+        console.log("Successful document update with docId: "+docId+" for orgId: "+orgId+" by userId: "+userId+" with role: "+orgRole);
+        return res.status(200).json({message: "Document updated successfully", data: updateQuery.rows[0]});
+
+    }catch(err){
+        if(err instanceof DatabaseError && err.code == "23505"){
+            return res.status(409).json({message: "Document name already exists"});
+        }
+        if(err instanceof DatabaseError && err.code == "23503"){
+            return res.status(400).json({message: "Invalid ancestorId"});
+        }
+        console.log("Error occured in document update. "+err);
+        return res.status(500).json({message: "Some error occured"});
+    }
 });
 
 export default router;
