1c567be5fca228f7230d223611d117d65837e3d3
Abhiraj028 <abhirajgautam028@gmail.com>
2026-03-03 22:14:01 +0530

    separated services layer
    f

--- backend/src/modules/workspaces/workspace.controllers.ts
+++ backend/src/modules/workspaces/workspace.controllers.ts
@@ -1,104 +1,41 @@
 import { Request, Response } from "express";
 import { workspaceCreationInterface, workspaceCreationSchema, workspaceDeleteParamsInterface, workspaceUpdateInterface, workspaceUpdateParamsInterface, workspaceUpdateSchema } from "./workspace.interfaces";
-import { checkWorkspaceMainRole, checkWorkspaceUpdateRole, createWorkspaceService, deleteWorkspaceService, fetchWorkspaceService } from "./workspace.services";
-import { nameCheckWorkspaceCreate, updateWorkspaceService, workspaceIdCheck } from "./workspace.services";
+import { createWorkspaceService, deleteWorkspaceService, fetchWorkspaceService, updateWorkspaceService } from "./workspace.services";
 import { reqCheck } from "../../shared/services";
-import { AppError } from "../../shared/errors";
 
 export const createWorkspace = async (req: Request<{}, {}, workspaceCreationInterface>, res: Response) => {
-    try {
-        const { userId, orgId, orgRole } = reqCheck(req.user);
-        checkWorkspaceMainRole(orgRole);
+    
+    const { userId, orgId, orgRole } = reqCheck(req.user);
+    const createBody = req.body;
 
-        const parsedBody =  workspaceCreationSchema.safeParse(req.body);
-        if(!parsedBody.success){
-            return res.status(400).json({message: "Invalid inputs: "+parsedBody.error.message});
-        }
-            
-        let workspaceName = nameCheckWorkspaceCreate(parsedBody.data.name);
+    const workspaceCreateServiceCall = await createWorkspaceService({userId, orgId, orgRole, createBody});
 
-        const workspaceRes = await createWorkspaceService(workspaceName, orgId, userId);
-        console.log("Workspace created successfully with name: "+workspaceName+" for orgId: "+orgId+ " by userId: "+userId+" workspaceId: "+workspaceRes.rows[0].id);
-
-        return res.status(201).json({ message: "Workspace created successfully", data: { name: workspaceName, workspaceId: workspaceRes.rows[0].id } });
-
-    } catch (err) {
-        console.log("Error in create workspace controller", err);
-        if(err instanceof AppError){
-            return res.status(err.status).json({message: err.message});
-        }
-
-        console.log("Unknown error in create workspace controller", err);
-        return res.status(500).json({ message: "Internal server error" });
-    }
+    return res.status(201).json({ message: "Workspace created successfully", data: workspaceCreateServiceCall });
 };
 
 export const fetchWorkspace = async (req: Request, res: Response) => {
-    try {
-        const { userId, orgId } = reqCheck(req.user);
 
-        const workspaceQuery = await fetchWorkspaceService(orgId);
-        console.log("Workspaces fetched successfully for orgId: "+orgId+" by userId: "+userId);
-
-        return res.status(200).json({ message: "Workspaces fetched successfully", data: workspaceQuery.rows });
-
-    } catch (err) {
-        console.log("Error in fetch workspace controller", err);
-        if(err instanceof AppError){
-            return res.status(err.status).json({message: err.message});
-        }
-
-        console.log("Unknown error in fetch workspace controller", err);
-        return res.status(500).json({ message: "Internal server error" });
-    }
+    const { orgId } = reqCheck(req.user);
+    const fetchWorkspaceServiceCall = await fetchWorkspaceService(orgId);
+    return res.status(200).json({ message: "Workspaces fetched successfully", data: fetchWorkspaceServiceCall });
 };
 
 export const deleteWorkspace = async (req: Request<workspaceDeleteParamsInterface>, res: Response) => {
-    try {
-        const { userId, orgId, orgRole } = reqCheck(req.user);
-        checkWorkspaceMainRole(orgRole);
 
-        const workspaceId = workspaceIdCheck(req.params.workspaceId);
+    const { userId, orgId, orgRole } = reqCheck(req.user);
+    const { workspaceId } = req.params;
 
-        await deleteWorkspaceService(workspaceId, orgId, userId);
-        console.log("Workspace deleted successfully with id: "+workspaceId+" by userId: "+userId);
-
-        return res.status(200).json({ message: "Workspace deleted successfully" });
-    } catch (err) {
-        console.log("Error in delete workspace controller", err);
-        if(err instanceof AppError){
-            return res.status(err.status).json({message: err.message});
-        }
-
-        console.log("Unknown error in delete workspace controller", err);
-        return res.status(500).json({ message: "Internal server error" });
-    }
+    const deleteWorkspaceServiceCall = await deleteWorkspaceService({userId, orgId, orgRole, workspaceId}); 
+    return res.status(200).json({ message: "Workspace deleted successfully" , data: deleteWorkspaceServiceCall});
 };
 
 export const updateWorkspace = async (req: Request<workspaceUpdateParamsInterface, {}, workspaceUpdateInterface>, res: Response) => {
-    try {
-        const { orgId, orgRole } = reqCheck(req.user);
-        checkWorkspaceUpdateRole(orgRole);
 
-        const workspaceId = workspaceIdCheck(req.params.workspaceId);
+    const { orgId, orgRole } = reqCheck(req.user);
+    const workspaceId = req.params.workspaceId
+    const updateBody = req.body;
 
-        const parsedData = workspaceUpdateSchema.safeParse(req.body);
-            if(!parsedData.success){
-                return res.status(400).json({message: "Invalid inputs: "+parsedData.error.message});
-            }
-        const workspaceName = parsedData.data.name;
+    const updateWorkspaceServiceCall = await updateWorkspaceService({ orgId, orgRole, workspaceId, updateBody});
 
-        await updateWorkspaceService(workspaceId, workspaceName, orgId);
-        console.log("Workspace updated successfully with id: "+workspaceId+" for orgId: "+orgId);
-
-        return res.status(200).json({ message: "Workspace updated successfully" , data: { workspaceId, name: workspaceName }});
-    } catch (err) {
-        console.log("Error in update workspace controller", err);
-        if(err instanceof AppError){
-            return res.status(err.status).json({message: err.message});
-        }
-
-        console.log("Unknown error in update workspace controller", err);
-        return res.status(500).json({ message: "Internal server error" });
-    }
+    return res.status(200).json({ message: "Workspace updated successfully" , data: updateWorkspaceServiceCall});
 };
--- backend/src/modules/workspaces/workspace.services.ts
+++ backend/src/modules/workspaces/workspace.services.ts
@@ -2,80 +2,101 @@ import Roles from "../../shared/enum";
 import crypto from "crypto";
 import { poolClient } from "../../db";
 import { DatabaseError } from "pg";
-import { AppError } from "../../shared/errors";
+import { AppError, BadRequestError, ConflictError, ForbiddenError, NotFoundError, ServerError } from "../../shared/errors";
+import { workspaceCreateServiceInterface, workspaceCreationSchema } from "./workspace.interfaces";
+import { workspaceDeleteServiceInterface, workspaceUpdateSchema, workspaceUpdateServiceInterface } from "./workspace.interfaces";
 
+export async function createWorkspaceService(props: workspaceCreateServiceInterface){
+    const { userId, orgId, orgRole, createBody } = props;
 
-export function checkWorkspaceMainRole(orgRole: string){
-    if(orgRole != Roles.ADMIN){
-        throw new AppError("Forbidden access", 403);
+    workspaceRoleCheck(orgRole);
+
+    const parsedBody = workspaceCreationSchema.safeParse(createBody);
+    if(!parsedBody.success){
+        throw new BadRequestError("Invalid inputs: "+parsedBody.error.message);
     }
-}
 
-export function nameCheckWorkspaceCreate(name: string){
+    let { name } = parsedBody.data;
     if(name.length == 0){
         name = "Default Workspace"+crypto.randomBytes(4).toString("hex");
     }
-    return name;
-}
-
-export function checkWorkspaceUpdateRole(orgRole: string){
-    if(orgRole == Roles.MEMBER){
-        throw new AppError("Forbidden access", 403);
-    }
-}
-
-export function workspaceIdCheck(givenId: string){
-    const workspaceId = Number(givenId);
-    if(isNaN(workspaceId) || workspaceId <= 0){
-        throw new AppError("Invalid workspaceId", 400);
-    }
-    return workspaceId;
-}
-
-export async function createWorkspaceService(workspaceName: string, orgId: number, userId: number){
+    
     try{
-        const workspaceRes = await poolClient.query("insert into workspaces(name, org_id, created_by) values($1,$2,$3) returning id",[workspaceName, orgId, userId]);
-        return workspaceRes;
+        const workspaceRes = await poolClient.query("insert into workspaces(name, org_id, created_by) values($1,$2,$3) returning *",[name, orgId, userId]);
+        return workspaceRes.rows[0];
     }catch(err){
         if(err instanceof DatabaseError && err.code === "23505"){
-            throw new AppError("Workspace with the same name already exists", 409);
+            throw new ConflictError("Workspace with the same name already exists");
         }
-        throw new AppError("Internal server error", 500);
-    }
+        throw err;
+    }    
 }
 
 export async function fetchWorkspaceService(orgId: number){
     const workspaceQuery = await poolClient.query("select id, name from workspaces where org_id = $1 and deleted_at is null", [orgId]);
-    return workspaceQuery;
+    return workspaceQuery.rows;
 }
 
-export async function updateWorkspaceService(workspaceId: number, workspaceName: string, orgId: number){
+export async function updateWorkspaceService(props: workspaceUpdateServiceInterface){
+    
+    const { orgId, orgRole, updateBody } = props;
+    const givenId = props.workspaceId;
+
+    workspaceRoleCheck(orgRole);
+
+    const workspaceId = Number(givenId);
+    if(isNaN(workspaceId) || workspaceId <= 0){
+        throw new BadRequestError("Invalid workspaceId");
+    }
+
+    const parsedBody = workspaceUpdateSchema.safeParse(updateBody);
+    if(!parsedBody.success){
+        throw new BadRequestError("Invalid inputs: "+parsedBody.error.message);
+    }
+    const { name } = parsedBody.data;
+
     try{
-        const updateWorkspaceQuery = await poolClient.query("update workspaces set name = $1, updated_at = now() where id = $2 and org_id = $3 and deleted_at is null returning id", [workspaceName, workspaceId, orgId]);
+        const updateWorkspaceQuery = await poolClient.query("update workspaces set name = $1, updated_at = now() where id = $2 and org_id = $3 and deleted_at is null returning *", [name, workspaceId, orgId]);
         if(updateWorkspaceQuery.rowCount == 0){
-            throw new AppError("Workspace not found", 404);
+            throw new NotFoundError("Workspace not found");
         }
-        return updateWorkspaceQuery;
+        return updateWorkspaceQuery.rows[0];
 
     }catch(err){
         if(err instanceof DatabaseError && err.code === "23505"){
-            throw new AppError("Workspace with the same name already exists", 409);
+            throw new ConflictError("Workspace with the same name already exists");
         }
-        throw new AppError("Internal server error", 500);
+        throw err;
     }    
 }
 
-export async function deleteWorkspaceService(workspaceId: number, orgId: number, userId: number){
+export async function deleteWorkspaceService(props: workspaceDeleteServiceInterface){
+    const { userId, orgId, orgRole } = props;
+    const givenId = props.workspaceId;
+    
+    workspaceRoleCheck(orgRole);
+
+    const workspaceId = Number(givenId);
+    if(isNaN(workspaceId) || workspaceId <= 0){
+        throw new BadRequestError("Invalid workspaceId");
+    }
+
     try{
-        const deleteQuery = await poolClient.query("update workspaces set deleted_at = now(), deleted_by = $1 where id = $2 and org_id = $3 and deleted_at is null returning id", [userId, workspaceId, orgId]);
+        const deleteQuery = await poolClient.query("update workspaces set deleted_at = now(), deleted_by = $1 where id = $2 and org_id = $3 and deleted_at is null returning *", [userId, workspaceId, orgId]);
         if(deleteQuery.rowCount == 0){
-            throw new AppError("Workspace not found", 404);
+            throw new NotFoundError("Workspace not found");
         }
-        return deleteQuery;
+        return deleteQuery.rows[0];
     }catch(err){
         if(err instanceof DatabaseError && err.code === "23503"){
-            throw new AppError("Cannot delete workspace with dependent records", 400);
+            throw new ConflictError("Cannot delete workspace with dependent records");
         }
-        throw new AppError("Internal server error", 500);
+        throw err;
     }   
+}
+
+function workspaceRoleCheck(role: string){
+    if(role != Roles.ADMIN){
+        throw new ForbiddenError("Forbidden access");
+    }
 }
